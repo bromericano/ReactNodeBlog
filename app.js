01f0c3333f34236a2ba9fe5354b053501f329936
Luke G <lukegronert@gmail.com>
2021-03-14 17:32:06 +0800

    Created sign up component and use React-Router to route user from sign in to sign up page by clicking sign up link

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 app.use(express.static(path.join(__dirname,"/html")));
+app.use(bodyParser.json());
+
+app.post('/signin', function (req, res) {
+    const user_name = req.body.email;
+    const password= req.body.password;
+    if(user_name=='admin' && password=='admin'){
+        res.send('success');
+    } else {
+        res.send('failure');
+    }
+});
 
 app.listen(7777, function() {
     console.log("Started listening on port", 7777);
--- html/main.jsx
+++ html/main.jsx
@@ -1,19 +1,86 @@
-class Signin extends React.Component {
+const Router = window.ReactRouter.Router;
+const Route = window.ReactRouter.Route;
+const hashHistory = window.ReactRouter.hashHistory;
+const Link = window.ReactRouter.Link;
+
+class Signup extends React.Component{
     render() {
         return (
-            <form className="form-signin">
-                <h2 className="form-signin-heading"> Please sign in </h2>
-                <label for="inputEmail" className="sr-only"> Email address </label>
-                <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
-                <label for="inputPassword" className="sr-only"> Password</label>
-                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                <button className="btn btn-lg btn-primary btn-block" type="button"> Sign in </button>
-            </form>
+            <div>
+                <form className="form-signin">
+                    <h2 className="form-signin-heading">Please sign up</h2>
+                    <label for="inputName" className="sr-only">Name</label>
+                    <input type="name" onChange={this.handleNameChange} id="inputName" className="form-control" placeholder="Name" required autofocus />
+                    <label for="inputEmail" className="sr-only">Email address</label>
+                    <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email" required autofocus />
+                    <label for="inputPassword" className="sr-only">Password</label>
+                    <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+
+                    <button className="btn btn-lg btn-primary btn-block" onClick={this.signUp} type="button">Sign Up</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+class Signin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.signIn = this.signIn.bind(this);
+        this.state = {
+            email:'',
+            password:''
+        };
+    }
+    handleEmailChange(e) {
+        this.setState({email:e.target.value})
+    }
+    handlePasswordChange(e) {
+        this.setState({password:e.target.value})
+    }
+    signIn(){
+        alert('Email address is ' + this.state.email + ' Password is ' + this.state.password);
+        axios.post('/signin', {
+            email: this.state.email,
+            password: this.state.password
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error)
+        });
+    }
+    render() {
+        return (
+            <div>
+                <form className="form-signin">
+                    <h2 className="form-signin-heading"> Please sign in </h2>
+                    <label for="inputEmail" className="sr-only"> Email address </label>
+                    <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+                    <label for="inputPassword" className="sr-only"> Password</label>
+                    <input type="passwprd" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+                    <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn} type="button"> Sign in </button>
+                </form>
+                <div>
+                    <Link to="/signup">{'Signup'}</Link>
+                </div>
+            </div>
         )
     }
 }
 
 ReactDOM.render( 
     <Signin /> ,
+    document.getElementById('app')
+);
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route component={Signin} path="/"></Route>
+        <Route component={Signup} path="/signup"></Route>
+    </Router>,
     document.getElementById('app')
 );
